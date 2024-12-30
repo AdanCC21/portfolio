f8ac8f28cd35bb3e5fbb927f50b91375f9297552
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2024-12-30 14:03:36 -0800

    icons in projects

--- src/components/ProjectItem.jsx
+++ src/components/ProjectItem.jsx
@@ -1,8 +1,27 @@
-export function ProjectItem({ imageName, title, children, theme, link }) {
+export function ProjectItem({ imageName, title, children, theme, link, array = [] }) {
+    const things = () => {
+        if (!Array.isArray(array) || array.length === 0) {
+            return null; // No hay elementos que renderizar
+        }
+
+        return (
+            <section className="project-card-tools">
+                {array.map((current, index) => (
+                    <img 
+                        src={`lenguage/${current}.svg`} 
+                        alt={current} 
+                        key={index} 
+                    />
+                ))}
+            </section>
+        );
+    };
+
     return (
-        <a className={`project-card ${theme}`} href={link} target="blank">
-            <img src={`project/${imageName}.png`} />
+        <a className={`project-card ${theme}`} href={link} target="_blank" rel="noopener noreferrer">
+            <img className="icon" src={`project/${imageName}.png`} alt={title} />
             <h1>{title}</h1>
+            {things()}
             <p>{children}</p>
         </a>
     );
