c20dced975603c789520fd132e9971138c14c274
AdanCC21 <141715477+AdanCC21@users.noreply.github.com>
2025-01-04 01:34:25 -0800

    bugs fixed in the home

--- src/components/TopBar.jsx
+++ src/components/TopBar.jsx
@@ -2,7 +2,7 @@ import '../css/topbar.css'
 
 export function TopBar({currentPage, setCurrentPage,theme}){
     return (
-        <div className='top-bar'>
+        <div className={`top-bar ${theme}`}>
             <section>
                 <a className={currentPage === 0 ? `selected ${theme}` : ''} onClick={() => setCurrentPage(0)}>Inicio</a>
                 <a className={currentPage === 1 ? `selected ${theme}` : ''}  onClick={() => setCurrentPage(1)}>Proyectos</a>
